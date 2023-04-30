ab2eec31f643955f71d3326ffe1e31dcd794abbb
StouAi <129324332+StouAi@users.noreply.github.com>
2023-04-30 21:04:09 +0300

    Update weeb_scraping.py

--- weeb_scraping.py
+++ weeb_scraping.py
@@ -23,15 +23,38 @@ def info(manga, check_tags=None):
         print(f'latest chapter link: {latest_chapt}\n')
     else:
         return
+
+
+#gets available  genres from webpage
+def getgenres():
+    html_genres = requests.get('https://manganato.com/genre-all?type=topview').text
+    genres_soup = bs(html_genres, 'lxml')
+    genres_cont = genres_soup.find('div', class_='container container-main')
+    genres_panel = genres_cont.find_all('a', class_='a-h text-nowrap')
+    # make genre list
+    genres_list = []
+    genre_count = 1
+    for genre in genres_panel:
+        title = genre.get('title')
+        if title:
+            genres_list.append(title.split("Manga")[0])
+    for elem in genres_list:
+        print(elem + ", ", end="")
+        if genre_count % 5 == 0:
+            print("")
+        genre_count += 1
+
 if __name__=="__main__":
     #gets url of main page
     html_text = requests.get('https://manganato.com/').text
     #we use soup to turn html to desired format and then we find use find
     soup = bs(html_text,'lxml')
     mangas = soup.find_all('div', class_='content-homepage-item')
-    print("Manga updates:")
+    print("Welcome this program print the latest manga updates")
+    print("These are all the available  genres\n")
+    getgenres()
     #get user input as a list
-    user_genres=input("Enter the tags you want to be included seperated by comma(,) or simply press Enter\n").split(',')
+    user_genres=input("\nEnter the tags you want to be included seperated by comma(,) or simply press Enter\n").split(',')
     f_user_tags=[user_tag.capitalize() for user_tag in user_genres ]
 
     for manga in mangas:
@@ -44,8 +67,8 @@ if __name__=="__main__":
             #find the genre tags
             tagger=tagsoup.find('div',class_='story-info-right')
             tagged=tagger.find_all('a',class_='a-h')
-            genre =tagged[1:]
-            genre_text=[tag.text for tag in genre]
+            genre_tag = tagged[1:]
+            genre_text=[tag.text for tag in genre_tag]
             #if user_genre are not in the manga add them to check_genre
             check_genre=[tag for tag in f_user_tags if tag not in genre_text]
             info(manga, check_genre)
